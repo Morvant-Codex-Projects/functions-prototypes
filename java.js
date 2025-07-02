8e3a11153e4d18d302f222383e00386aa8a5eb58
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-02 02:01:05 -0500

    connected js and css to html completed first function for Alice

--- java.js
+++ java.js
@@ -1,9 +1,13 @@
-function Person (name, age, eyeColor, birthday ) {
+function Person (name, age, eyeColor, hasPets ) {
     this.name = name; //string
     this.age = age; //number
     this.eyeColor = eyeColor; //string
-    this.birthday = birthday; //date
+    this.hasPets = hasPets; //boolean
 }
 
 
-const myFriend = new Person(Alicec)
+const myFriend = new Person("Alice", 30, "green", true);
+
+console.log(`Hello ${myFriend.name} welcome to ${myFriend.age}!`);
+
+console.log(myFriend);
