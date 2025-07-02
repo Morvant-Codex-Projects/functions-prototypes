7a26cce8e2bbf15d8cdde929332dfb1f9d0b706c
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-01 19:27:11 -0500

    add header and js

---
+++ java.js
@@ -0,0 +1,9 @@
+function Person (name, age, eyeColor, birthday ) {
+    this.name = name; //string
+    this.age = age; //number
+    this.eyeColor = eyeColor; //string
+    this.birthday = birthday; //date
+}
+
+
+const myFriend = new Person
