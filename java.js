eccee736dc4fbe8c24b16660790a8fda3ed5aa7b
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-07-02 02:10:43 -0500

    Create an instance of the Person class

--- java.js
+++ java.js
@@ -11,3 +11,17 @@ const myFriend = new Person("Alice", 30, "green", true);
 console.log(`Hello ${myFriend.name} welcome to ${myFriend.age}!`);
 
 console.log(myFriend);
+
+
+class Person1{
+  constructor(name, age, eyeColor, hasPets) {
+    this.name = name; //string
+    this.age = age; //number
+    this.eyeColor = eyeColor; //string
+    this.hasPets = hasPets; //boolean
+}
+}
+
+const Alice = new Person("Alice", 30, "green", true);
+
+console.log(`Hello ${Alice.name} welcome to ${Alice.age}!`);
